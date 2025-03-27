d98a0c6aaa7b570287a292eaee691ac06111f1ba
Volanalytics <bpmoran@protonmail.com>
2025-03-27 16:57:16 -0500

    Create index.jsx

---
+++ prototypes/src/components/ui/index.jsx
@@ -0,0 +1,55 @@
+// Export all UI components from this file for easy importing
+
+// Button
+export { Button } from './button';
+
+// Card
+export { 
+  Card, 
+  CardHeader, 
+  CardTitle, 
+  CardDescription, 
+  CardContent, 
+  CardFooter 
+} from './card';
+
+// Tabs
+export { 
+  Tabs, 
+  TabsList, 
+  TabsTrigger, 
+  TabsContent 
+} from './tabs';
+
+// Badge
+export { Badge } from './badge';
+
+// Avatar
+export { 
+  Avatar, 
+  AvatarImage, 
+  AvatarFallback 
+} from './avatar';
+
+// Input
+export { Input } from './input';
+
+// Dialog
+export { 
+  Dialog, 
+  DialogTrigger, 
+  DialogContent, 
+  DialogHeader, 
+  DialogTitle, 
+  DialogDescription, 
+  DialogFooter 
+} from './dialog';
+
+// Select
+export { 
+  Select, 
+  SelectTrigger, 
+  SelectValue, 
+  SelectContent, 
+  SelectItem 
+} from './select';
